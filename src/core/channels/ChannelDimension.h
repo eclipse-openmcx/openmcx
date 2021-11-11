0ef198f5e7b720056ac9edee0f0d300e349d4d5d
Denis Čaušević <denis.causevic@avl.com>
2021-11-11 14:58:54 +0100

    Add a function to check whether dimensions are conformable (same sizes)

--- src/core/channels/ChannelDimension.h
+++ src/core/channels/ChannelDimension.h
@@ -34,6 +34,7 @@ ChannelDimension * ChannelDimensionClone(ChannelDimension * dimension);
 size_t ChannelDimensionNumElements(ChannelDimension * dimension);
 ChannelDimension * ChannelDimensionClone(ChannelDimension * dimension);
 int ChannelDimensionEq(ChannelDimension * first, ChannelDimension * second);
+int ChannelDimensionConformable(ChannelDimension * first, ChannelDimension * second);
 int ChannelDimensionIncludedIn(const ChannelDimension * first, const ChannelDimension * second);
 size_t ChannelDimensionGetIndex(ChannelDimension * dimension, size_t elem_idx, size_t * sizes);
 char * ChannelDimensionString(const ChannelDimension * dimension);
